31f9cb1d050efd5afe2b9564cc1a2238e15e5d96
Leonidasdim <Leondiasdimakis2@gmail.com>
2021-11-28 21:26:22 +0200

    Add project files.

---
+++ Minimal network/NetworkKruskal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimal_network
+{
+    
+    class NetworkKruskal
+    {
+
+        public int[][] Netwrok()
+
+
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string path = Path.Combine(Directory.GetParent(workingDirectory).Parent.FullName, @"network\", @"_network.txt");
+            //whole to a single string
+            string text = File.ReadAllText(path);
+
+            /* split and input on 2d array*/
+
+            var Network = text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+
+                .Select(line => line.Trim()
+                    .Split(',')
+                    .Select(s => int.Parse(s.Replace('-','0')))        //if - set 0 
+                    .ToArray())
+               .ToArray();
+
+            return Network;
+        }
+    }
+}
---
+++ Minimal network/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* https://projecteuler.net/problem=107*/
+
+/*Calculate Minimum Spanning Tree with kruskal*/
+
+namespace Minimal_network
+{
+    class Program
+    {
+		
+		static void Main(string[] args)
+        {
+            
+            NetworkKruskal mynet = new NetworkKruskal();
+
+            var kurskalGraph = mynet.Netwrok();
+
+            int Edges = 0;
+
+            for (int i = 0; i <= kurskalGraph.Length - 1; i++)
+                for (int j = i+1; j <= kurskalGraph.Length - 1; j++)
+                {
+                    if (kurskalGraph[i][j] != 0)
+                        Edges++;                         //calculate number of Edges
+                }
+
+
+             Graph graph = new Graph(kurskalGraph.Length, Edges);
+
+
+            int k = 0;
+
+            for (int i = 0; i <= kurskalGraph.Length - 1; i++)
+                for (int j = i+1; j  <= kurskalGraph.Length - 1; j++)
+                {
+                    if (kurskalGraph[i][j] != 0)                    // if have  Edge    
+                    {
+                        graph.edge[k].src = i;                                 // intialized values of graph
+                        graph.edge[k].dest = j;
+                        graph.edge[k].weight = kurskalGraph[i][j];
+                        k++;
+                    }
+                }
+
+            graph.KruskalMST();
+
+            Console.ReadLine();
+        }
+    }
+}
